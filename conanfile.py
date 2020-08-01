1bd5a31274ba40d6f484db9e426802f8a2c029f1
Karl Wallner <kwallner@mail.de>
2020-08-01 16:38:52 +0200

    Update to 2.5.0.

--- conanfile.py
+++ conanfile.py
@@ -1,28 +1,46 @@
 from conans import ConanFile, tools, CMake
+import os
 
 
 class PyBind11Conan(ConanFile):
     name = "pybind11"
-    version = "2.2.4"
-    settings = "os", "compiler", "arch", "build_type"
+    version = "2.5.0"
     description = "Seamless operability between C++11 and Python"
+    topics = "conan", "pybind11", "python", "binding"
     homepage = "https://github.com/pybind/pybind11"
-    license = "BSD Style: https://github.com/pybind/pybind11/blob/master/LICENSE"
+    license = "BSD-3-Clause"
     url = "https://github.com/conan-community/conan-pybind11"
-    no_copy_sources = True
-
+    exports_sources = "CMakeLists.txt"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake"
+    no_copy_source = True
+    
+    def requirements(self):
+        self.requires("python/3.7.8@%s/%s" % (self.user, self.channel))
+        
     def source(self):
         tools.get("%s/archive/v%s.tar.gz" % (self.homepage, self.version))
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["PYBIND11_INSTALL"] = True
         cmake.definitions["PYBIND11_TEST"] = False
+        cmake.definitions["PYBIND11_CMAKECONFIG_INSTALL_DIR"] = "lib/cmake/pybind11"
         cmake.configure(source_folder="pybind11-%s" % self.version)
         cmake.build()
         cmake.install()
 
     def package(self):
-        self.copy("*LICENSE", keep_path=False)
-  
+        self.copy("LICENSE")
+        os.unlink(os.path.join(self.package_folder, "lib", "cmake", "pybind11", "pybind11Config.cmake"))
+        os.unlink(os.path.join(self.package_folder, "lib", "cmake", "pybind11", "pybind11ConfigVersion.cmake"))
+
     def package_id(self):
         self.info.header_only()
+
+    def package_info(self):
+        self.cpp_info.includedirs.append(os.path.join(self.package_folder, "include", "pybind11"))
+        cmake_base_path = os.path.join("lib", "cmake", "pybind11")
+        self.cpp_info.builddirs = [cmake_base_path]
+        self.cpp_info.build_modules = [os.path.join(cmake_base_path, "FindPythonLibsNew.cmake"),
+                                       os.path.join(cmake_base_path, "pybind11Tools.cmake")]
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,19 +1,32 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 import os
 import sys
+import shutil
 
 
-class Pybind11TestConan(ConanFile):
+class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package"
+
+    def requirements(self):
+        self.requires("python/3.7.8@%s/%s" % (self.user, self.channel))
+
+    @property 
+    def _python_interpreter(self):
+        python_cmd = shutil.which("python3")
+        if python_cmd is None:
+            python_cmd = shutil.which("python")
+        if python_cmd is None:
+            raise errors.ConanInvalidConfiguration("Failed to find python executable.")
+        return python_cmd
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["PYTHON_EXECUTABLE"] = self._python_interpreter
         cmake.configure()
         cmake.build()
 
     def test(self):
-        sys.path.append(".")
-        import example
-        self.output.info("Add %s" % example.add(2, 3))
-        assert example.add(2, 40) == 42
+        if not tools.cross_building(self.settings):
+            with tools.environment_append({"PYTHONPATH": "lib"}):
+                self.run("{} {}".format(self._python_interpreter, os.path.join(self.source_folder, "test.py")), run_environment=True)
